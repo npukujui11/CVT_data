37cb4e2b2abe4c5812afbc8389784b548ef373cf
npukujui11 <npukujui11@gmail.com>
2020-03-27 15:28:15 +0800

    初始化蓝牙模块功能

--- app/src/main/java/com/example/cvt_data/Bluetoothfunction.java
+++ app/src/main/java/com/example/cvt_data/Bluetoothfunction.java
@@ -6,8 +6,11 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,8 +39,45 @@ public class Bluetoothfunction extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Bluetooth device not available", Toast.LENGTH_LONG).show();
             finish();
         }else if(!myBluetooth.isEnabled()){
-
+            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBTon, 1);
         }
+        
+        btnPaired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    pairedDevicesList();
+            }
+        });
 
     }
+    
+    private void pairedDevicesList(){
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+        
+        if(pairedDevices.size() > 0) {
+            for(BluetoothDevice bt : pairedDevices){
+                list.add(bt.getName().toString() + "\n" + bt.getAddress().toString());
+            }
+        }else{
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+        
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.activity_list_item, list);
+        devicelist.setAdapter(adapter);
+        devicelist.setOnItemClickListener(myListClickListener);
+    }
+
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String info = ((TextView) view).getText().toString();
+            String address = info.substring(info.length()-17);
+
+            //Intent i = new Intent(Bluetoothfunction.this, ledControl.class);
+            //i.putExtra(EXTRA_ADDRESS, address);
+            //startActivity(i);
+        }
+    };
 }
